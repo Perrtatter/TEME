dd1e3bd305e7e1e7100b889d3d090196fc6fbc8e
Perrtatter <159263635+Perrtatter@users.noreply.github.com>
2026-03-09 21:57:50 +0100

    add gui 1er

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 # import module
+from tkinter import *
+from tkinter import PhotoImage
+
 from src.client import Client
 from src.server import Server
 
 
-# one windows from the listener 
-# one windows from the sender 
-
 
 # setup client / server
 client = Client(username="DEV",ip="localhost")
-server = Server(host="localhost",port=4444)
+server = Server(host="localhost",port=4444) 
+
+
+# windows config
+win = Tk()
+win.iconbitmap("icon.ico")
+win.title("TEME Client")
+win.geometry("200x200")
+win.resizable(False,False)
+
+# Load the logo 
+image = PhotoImage(file="logo.png")
+image_label = Label(win, image=image)
+image_label.pack()
+
+# run listener button
+listener_button = Button(win,text="Listener")
+listener_button.pack()
+
+# run sender button
+sender_button = Button(win,text="Sender")
+sender_button.pack()
+
+win.mainloop()
 
 # send from client
 # client.send_message("First message of TEME","localhost",4444)
 
-# run server
+
+'''# infinte loop
 while True: 
-    recv_data = server.blind_server()
-    print(recv_data)
+    # run server to get data
+    recv_data = server.blind_server()'''
+    
+    
